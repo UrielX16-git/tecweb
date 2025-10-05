49cafd7008aa37ee581f69ccfa068fcf33cfbdac
UrielX16-git <leiru_uriel16@hotmail.com>
2025-10-05 14:21:56 -0600

    Estrucutra base de los ejemplos

--- actividades/a06/src/main.js
+++ actividades/a06/src/main.js
@@ -1,13 +1,32 @@
-
-function getDatos()
-{
-    var nombre = prompt("Nombre: ", "");
-
-    var edad = prompt("Edad: ", 0);
-
-    var div1 = document.getElementById('nombre');
-    div1.innerHTML = '<h3> Nombre: '+nombre+'</h3>';
-
-    var div2 = document.getElementById('edad');
-    div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
+function ejemplo1() {
+    document.getElementById("resultado1").innerHTML = "Hola Mundo";
 }
+
+function ejemplo2() {
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+    var resultado = "Nombre: " + nombre + "<br>" +
+                    "Edad: " + edad + "<br>" +
+                    "Altura: " + altura + "<br>" +
+                    "Casado: " + casado;
+    document.getElementById("resultado2").innerHTML = resultado;
+}
+
+function ejemplo3() {}
+function ejemplo4() {}
+function ejemplo5() {}
+function ejemplo6() {}
+function ejemplo7() {}
+function ejemplo8() {}
+function ejemplo9() {}
+function ejemplo10() {}
+function ejemplo11() {}
+function ejemplo12() {}
+function ejemplo13() {}
+function ejemplo14() {}
+function ejemplo15() {}
+function ejemplo16() {}
+function ejemplo17() {}
+function ejemplo18() {}
